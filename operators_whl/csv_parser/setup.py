68583c7e36fb15e5d348724610cb5e54b7ec23d9
zhouziang <zhouziang1996@outlook.com>
2025-07-04 11:12:58 +0800

    add_5

---
+++ operators_whl/csv_parser/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cbf-processor',
+    version='0.1.0',
+    packages=find_packages(),
+    description='CBF file handler for operator framework',
+    author='Your Name',
+    install_requires=[],  # 可添加如 numpy 等依赖
+    python_requires='>=3.6',
+)
